92bbac0168b3ec3a6cd2a7daeab5e9977b667a07
Steven Gong <stevengong@stevens-mbp-2.lan>
2022-11-10 23:22:06 +1100

    ng cli new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  //bootstrap: [],
+  entryComponents: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const element = createCustomElement(AppComponent, {
+      injector: this.injector,
+    });
+    customElements.define('app-micro-fe', element);
+  }
+}
